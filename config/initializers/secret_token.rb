8ee48d38c82ca84239bd57c316c404b7130735ec
Cloud Le <lehaoson@gmail.com>
2014-03-31 13:49:56 +0700

    Initialize the project.
    Installed bundle, configure database connection using local password.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Zeprj::Application.config.secret_key_base = '100dd9d642f7093a3fc89a7ddd8b687eb15770add0d6e75f6d4e3c7f76c000a6bf1628aa75dc02ccee0f17383f3dcc8c51f3c9bd34e0c5f145ad63034bbf421d'
