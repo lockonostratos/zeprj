c14eb3be0e48488e27c230f4d14e489c360f9aae
Cloud Le <lehaoson@gmail.com>
2014-04-17 19:38:32 +0700

    Fix account-create's before filter.

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,18 +1,16 @@
 class Account < ActiveRecord::Base
   has_secure_password #secret here!
-  before_create { generate_token(:auth_token) }
-
+  before_create { [generate_token(:auth_token), ] }
+  after_create { add_to_merchant }
   def generate_token(column)
     begin
       self[column] = SecureRandom.urlsafe_base64
     end while Account.exists?(column => self[column])
   end
 
-  before_create :add_to_merchant
-
   private
   def add_to_merchant
-   mer= Merchant.create! ({name:"abcd", headquater:12})
+   mer= Merchant.create! ({name:"abcd", headquater:self.id})
    bran= MerchantBranche.create! ({merchant_id:mer.id, name:"asdadas"})
    MerchantWarehouse.create! ({merchant_branche_id:bran.id, name:"asdasdas"})
   end
